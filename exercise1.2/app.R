c07808f9f4e8fd3e28b81c311e9e073b8b7d2651
Zee Setash <azsetash@gmail.com>
2023-08-29 12:44:46 -0400

    update to have variable y

--- exercise1.2/app.R
+++ exercise1.2/app.R
@@ -3,13 +3,14 @@ library(shiny)
 
 ui <- fluidPage(
   sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
-  "then x times 5 is",
+  sliderInput("y", label = "If y is", min = 1, max = 50, value = 30),
+  "then x times y is",
   textOutput("product")
 )
 
 server <- function(input, output, session) {
   output$product <- renderText({ 
-    input$x * 5
+    input$x * input$y
   })
 }
 
