a0dc4095f8583b80c8e492b579e1ccd10615f207
Zee Setash <azsetash@gmail.com>
2023-08-29 13:01:26 -0400

    modify to have addition

--- exercise1.2/app.R
+++ exercise1.2/app.R
@@ -4,13 +4,23 @@ library(shiny)
 ui <- fluidPage(
   sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
   sliderInput("y", label = "If y is", min = 1, max = 50, value = 30),
-  "then x times y is",
-  textOutput("product")
+  "then, (x * y) is", textOutput("product"),
+  "and, (x * y) + 5 is", textOutput("product_plus5"),
+  "and (x * y) + 10 is", textOutput("product_plus10")
 )
 
 server <- function(input, output, session) {
-  output$product <- renderText({ 
+  product <- reactive({
     input$x * input$y
+    })
+    output$product <- renderText({ 
+      product()
+  })
+  output$product_plus5 <- renderText({ 
+    product() + 5
+  })
+  output$product_plus10 <- renderText({ 
+    product() + 10
   })
 }
 
