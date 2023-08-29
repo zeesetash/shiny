dedfb1833cfd5ecb65f62cb1433a57a70f8a0d5b
Zee Setash <azsetash@gmail.com>
2023-08-29 12:21:02 -0400

    update to use dataset

--- demo/app.R
+++ demo/app.R
@@ -9,8 +9,24 @@
 
 library(shiny)
 ui <- fluidPage(
-  "Hello, world!"
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
 )
 server <- function(input, output, session) {
+  # Create a reactive expression
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
+  output$summary <- renderPrint({
+    # Use a reactive expression by calling it like a function
+    summary(dataset())
+  })
+  
+  output$table <- renderTable({
+    dataset()
+  })
 }
+
 shinyApp(ui, server)
