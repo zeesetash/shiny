8e2e64fcf16b8cbcf03acb63389fb5e646d02a49
Zee Setash <azsetash@gmail.com>
2023-08-30 15:04:51 -0400

    create Er_injury

---
+++ exercises/ER_injury.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(vroom)
+library(tidyverse)
+
+dir.create("neiss")
+#> Warning in dir.create("neiss"): 'neiss' already exists
+download <- function(name) {
+  url <- "https://github.com/hadley/mastering-shiny/raw/master/neiss/"
+  download.file(paste0(url, name), paste0("neiss/", name), quiet = TRUE)
+}
+download("injuries.tsv.gz")
+download("population.tsv")
+download("products.tsv")
