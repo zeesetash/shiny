157e657cda12f8e6f5fb05bbebe2646bcc8b3c9e
Zee Setash <azsetash@gmail.com>
2023-08-30 15:18:51 -0400

    create injury.R remove duplicate

---
+++ injury/injury.R
@@ -0,0 +1,74 @@
+library(shiny)
+library(tidyverse)
+
+
+
+#begin app
+prod_codes <- setNames(products$prod_code, products$title)
+
+ui <- fluidPage(
+  fluidRow(
+    column(8,
+           selectInput("code", "Product",
+                       choices = setNames(products$prod_code, products$title),
+                       width = "100%"
+           )
+    ),
+    column(2, selectInput("y", "Y axis", c("rate", "count"))
+           )
+  ),
+  fluidRow(
+    column(4, tableOutput("diag")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
+  ),
+  fluidRow(
+    column(12, plotOutput("age_sex"))
+  ),
+  fluidRow(
+    column(2, actionButton("story", "Tell me a story")),
+    column(10, textOutput("narrative"))
+  )
+)
+server <- function(input, output, session) {
+  count_top <- function(df, var, n = 5) {
+    df %>%
+      mutate({{ var }} := fct_lump(fct_infreq({{ var }}), n = n)) %>%
+      group_by({{ var }}) %>%
+      summarise(n = as.integer(sum(weight)))
+  }
+  
+  selected <- reactive(injuries %>% filter(prod_code == input$code))
+  
+  output$diag <- renderTable(count_top(selected(), diag), width = "100%")
+  output$body_part <- renderTable(count_top(selected(), body_part), width = "100%")
+  output$location <- renderTable(count_top(selected(), location), width = "100%")
+  
+  summary <- reactive({
+    selected() %>%
+      count(age, sex, wt = weight) %>%
+      left_join(population, by = c("age", "sex")) %>%
+      mutate(rate = n / population * 1e4)
+  })
+  
+  output$age_sex <- renderPlot({
+    if (input$y == "count") {
+      summary() %>%
+        ggplot(aes(age, n, colour = sex)) +
+        geom_line() +
+        labs(y = "Estimated number of injuries in US")
+    } else {
+      summary() %>%
+        ggplot(aes(age, rate, colour = sex)) +
+        geom_line(na.rm = TRUE) +
+        labs(y = "Injuries per 10,000 Americans")
+    }
+  }, res = 96)
+  narrative_sample <- eventReactive(
+    list(input$story, selected()),
+    selected() %>% pull(narrative) %>% sample(1)
+  )
+  output$narrative <- renderText(narrative_sample())
+}
+ 
+shinyApp(ui = ui, server = server)
